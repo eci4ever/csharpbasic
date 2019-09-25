158f05e27ffffc815db30faa213dd9a28695a67c
nmfairus <nmfairus@live.com>
2019-09-25 08:45:48 +0800

    Fix implicitly called constructors

--- constructor1/ImplicitConstructor.cs
+++ constructor1/ImplicitConstructor.cs
@@ -1,6 +1,10 @@
-public class ImplicitConstructor
+class ImplicitConstructor
 {
     int i;
     string s;
     bool b;
+    public ImplicitConstructor()
+    {
+        System.Console.WriteLine("int {0}, string {1}, bool{2}", i, s, b);
+    }
 }
--- constructor1/Program.cs
+++ constructor1/Program.cs
@@ -4,15 +4,10 @@ namespace constructor1
 {
     class Program
     {
-        int a = 2; string b=null; bool c=true;
         static void Main(string[] args)
         {
-            Program p = new Program();
-            System.Console.WriteLine(p.a);
-            System.Console.WriteLine(p.b);
-            System.Console.WriteLine(p.c);
-            //
-            Console.WriteLine("Hello World!");
+            ImplicitConstructor imp = new ImplicitConstructor();
+            System.Console.WriteLine("Hello");
         }
     }
 }
