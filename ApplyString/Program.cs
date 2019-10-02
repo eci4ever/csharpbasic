6b3f5b7f1abffbcf28a46dcbf40bde0e763cc04b
nmfairus <nmfairus@live.com>
2019-10-02 08:51:32 +0800

    Add type of variable

---
+++ ApplyString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ApplyString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string noKP = "790427115413";
+            string year = noKP.Substring(0,2);
+            string month = noKP.Substring(2,2);
+            string day = noKP.Substring(4,2);
+            string bday = month + "-" + day + "-" + year;
+            DateTime parsedDate;
+            DateTime.TryParseExact(bday, "MM-dd-yy", null, DateTimeStyles.None, out parsedDate);
+            //DateTime mydate = DateTime.Parse(bday);
+            System.Console.WriteLine(parsedDate);
+            Console.WriteLine(bday);
+        }
+    }
+}
---
+++ varType/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace varType
+{
+    class Program
+    {
+        public Program(int x)
+        {
+            this.c = x;
+        }
+        int a = 100;
+        static int b = 200;
+        const float pi = 3.14f;
+        readonly int c;
+        static void Main(string[] args)
+        {
+            Program p = new Program(10);
+            
+            System.Console.WriteLine(p.a);
+            System.Console.WriteLine(b);
+            System.Console.WriteLine(p.c);
+            System.Console.WriteLine(pi);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
