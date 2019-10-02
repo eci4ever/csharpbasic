97c2d0729339c7a435274a4c43443dbe8d06a1aa
nmfairus <nmfairus@live.com>
2019-10-02 09:04:35 +0800

    Add inheritance part 1

---
+++ inheritanceP1/Class1.cs
@@ -0,0 +1,14 @@
+namespace inheritanceP1
+{
+    public class Class1
+    {
+        void Test1()
+        {
+            System.Console.WriteLine("Test1 called");
+        }
+        public void Test2()
+        {
+            System.Console.WriteLine("Test2 called");
+        }
+      }
+}
---
+++ inheritanceP1/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace inheritanceP1
+{
+    class Class2 : Class1
+    {
+        public void Test3()
+        {
+            System.Console.WriteLine("Test3 called");
+        }
+        static void Main(string[] args)
+        {
+            Class2 c2 = new Class2();
+            //c2.Test1(); c2.Test1() can't be called because dont have public access specifier
+            c2.Test2();
+            c2.Test3();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
