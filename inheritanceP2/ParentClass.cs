5c21800360abe2cb6e6da208df7072ed9414dd06
nmfairus <nmfairus@live.com>
2019-10-03 08:53:10 +0800

    Learn inheritance part 2

---
+++ inheritanceP2/ParentClass.cs
@@ -0,0 +1,10 @@
+namespace inheritanceP2
+{
+    public class ParentClass
+    {
+        public ParentClass(int i)
+        {
+            System.Console.WriteLine("Parent constructor is called " + i);
+        }
+    }
+}
---
+++ inheritanceP2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inheritanceP2
+{
+    class Program : ParentClass
+    {
+        public Program(int x) : base(x)
+        {
+            System.Console.WriteLine("Program constructor is called");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program(20);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
