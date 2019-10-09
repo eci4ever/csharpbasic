48b6188d6a956e3f0a6632f5b34e7a49463332e4
nmfairus <nmfairus@live.com>
2019-10-09 09:56:44 +0800

    Learn polymorphism

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        public void Test()
+        {
+            System.Console.WriteLine("1st Method Called");
+        }
+
+        public void Test(int i)
+        {
+            System.Console.WriteLine("2nd Method Called");
+        }
+
+        public void Test(string s)
+        {
+            System.Console.WriteLine("3rd Method Called");
+        }
+
+        public void Test(int i, string s)
+        {
+            System.Console.WriteLine("4th Method Called");
+        }
+
+        public void Test(string s, int i)
+        {
+            System.Console.WriteLine("5th Method Called");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Test();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MethodOverridding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MethodOverridding
+{
+    class ParentClass
+    {
+        public void Show()
+        {
+            System.Console.WriteLine("Parent Show Method called");
+        }
+
+        public virtual void Test()
+        {
+            System.Console.WriteLine("Parent Test method is called!!");
+        }
+    }
+    class Program : ParentClass
+    {
+        public override void Test()
+        {
+            System.Console.WriteLine("Test Child class is called");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Test();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
