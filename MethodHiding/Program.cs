7392cf6bb24b37e3aa829355098dceb05e3f9f1a
nmfairus <nmfairus@live.com>
2019-10-10 12:01:25 +0800

    Method hiding

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MethodHiding
+{
+    class ParentClass
+    {
+        public virtual void Test1()
+        {
+            System.Console.WriteLine("Parent tesr one called");
+        }
+         public void Show1()
+        {
+            System.Console.WriteLine("Parent show method is called");
+        }
+    }
+    class Program : ParentClass
+    {
+        public override void Test1()
+        {
+            System.Console.WriteLine("Child test 1 called");
+        }
+        public new void Show1()
+        {
+            System.Console.WriteLine("child show1 is called");
+        }
+        public void PTest1()
+        {
+            base.Test1();
+        }
+        public void PShow1()
+        {
+            base.Show1();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            ParentClass pl = p;
+            pl.Test1();
+            pl.Show1();
+            // p.Show1();
+            // p.Test1();
+            // p.PShow1();
+            // p.PTest1();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
