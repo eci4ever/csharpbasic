abc9ca1ee7694457ed1b49dd7f4ed772d1d15057
eci4ever <eci4ever@gmail.com>
2019-10-24 11:20:43 +0800

    Learn Struct

---
+++ Structure/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Structure
+{
+    struct MyStruct
+    {
+        int i;
+        public MyStruct(int x)
+        {
+            this.i = x;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Struct is Called!!" + i);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStruct my = new MyStruct();
+            my.Display();
+            MyStruct my1 = new MyStruct(20);
+            my1.Display();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
