7fb551907bdc3606085b83581ab26aea5ca9e0e6
eci4ever <eci4ever@gmail.com>
2019-10-30 09:03:35 +0800

    Learn enumeration and properties

---
+++ Enumeration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enumeration
+{
+    public enum WeekDays 
+    { 
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+    class Program
+    {
+        public static WeekDays MeetingDay { get; set; } = WeekDays.Thursday;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MeetingDay);
+            MeetingDay = WeekDays.Friday;
+            Console.WriteLine(MeetingDay);
+
+            foreach(int i in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(i + " : " + (WeekDays)i);
+            }
+            foreach(string s in Enum.GetNames(typeof(WeekDays)))
+            {
+                Console.WriteLine(s);
+            }
+            WeekDays d = (WeekDays)1;
+            Console.WriteLine(d);
+            WeekDays d2 = WeekDays.Thursday;
+            Console.WriteLine(d2);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PropertiesP1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PropertiesP1
+{
+    class RadiusClass
+    {
+        double _Radius  = 12.03;
+        public double Radius 
+        {
+            get { return _Radius;}
+            set { _Radius = value;}
+        }
+    }
+
+    class CutiClass
+    {
+        public string Cuti { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RadiusClass r = new RadiusClass();
+            Console.WriteLine(r.Radius);
+            r.Radius = 15.05;
+            Console.WriteLine(r.Radius);
+            Console.WriteLine("Hello World!");
+            CutiClass c = new CutiClass();
+            c.Cuti = "Thursday";
+            Console.WriteLine(c.Cuti);
+        }
+    }
+}
