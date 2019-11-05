3afc0686f67bcf4ef840d5bafb1fda956adae164
eci4ever <eci4ever@gmail.com>
2019-11-05 08:50:13 +0800

    Learn delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegate
+{
+    public delegate void AddDelegate(int x, int y);
+    public delegate string SayDelegate(string str);
+    class Program
+    {
+        public void AddNum(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public static string SayHello(string name)
+        {
+            return $"hello {name}";
+        }
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+
+            AddDelegate ad = new AddDelegate(pr.AddNum);
+            ad.Invoke(10,20);
+            ad(20,30);
+            SayDelegate sd = new SayDelegate(SayHello);
+            Console.WriteLine(sd.Invoke("Test"));
+            Console.WriteLine(sd("Test2"));
+            // pr.AddNum(5,3);
+            // Console.WriteLine(SayHello("nmfairus"));
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MultiDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultiDelegate
+{
+    public delegate void RectDelegate(int a, int b);
+    class RectClass
+    {
+        public void GetArea(int a , int b)
+        {
+            Console.WriteLine($"Area is : {a * b}");
+        }
+        public void GetPerimeter(int a, int b)
+        {
+            Console.WriteLine($"Perimeter is : {2 * a * b}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RectClass rc = new RectClass();
+            RectDelegate rd = new RectDelegate(rc.GetArea);
+            rd += rc.GetPerimeter;
+
+            rd.Invoke(5,4);
+            // rc.GetArea(4,5);
+            // rc.GetPerimeter(4,5);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
