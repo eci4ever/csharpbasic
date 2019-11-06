5d64dccea4e40008844b4b0243a8882b3252fc33
eci4ever <eci4ever@gmail.com>
2019-11-06 09:36:48 +0800

    Learn anonymous method

---
+++ AnonMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnonMethod
+{
+    public delegate string GreetingDelegate(string str);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetingDelegate obj = delegate(string name)
+            {
+                return "Hello " + name + ", Good Morning";
+            };
+
+            var str = obj.Invoke("eci");
+            Console.WriteLine(str);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
