f7ee7a7be697ba331c32081eb625eb8bf70e29dc
eci4ever <eci4ever@gmail.com>
2019-11-07 09:04:03 +0800

    Learn Lambda

---
+++ GenericDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GenericDelegate
+{
+    class Program
+    {
+        public static double Add1(int x, float y, double z)
+        {
+            return x * y * z;
+        }
+        public static void Add2(int x, float y, double z)
+        {
+            Console.WriteLine(x * y * z);
+        }
+        public static bool CheckLenght(string str)
+        {
+            return (str.Length > 5)?true:false;
+        }
+        static void Main(string[] args)
+        {
+            Func<int,float,double,double> obj = Add1;
+            double result1 = obj.Invoke(2,4.2f,5.1);
+            Console.WriteLine(result1);
+            Action<int,float,double> obj2 = Add2;
+            obj2.Invoke(2,4.2f,5.1);
+            Predicate<string> obj3 = CheckLenght;
+            bool result2 = obj3.Invoke("Hello");
+            Console.WriteLine(result2);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LambdaExpression
+{
+    public delegate string GreetingDelegate(string str);
+    public delegate void SayHello();
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            GreetingDelegate obj = (name) => //Using arrow function then remove keyword delegate, datatype for parameter also removed
+            {
+                return "Hello " + name + ", Good Morning";
+            };
+            var str = obj.Invoke("nmfairus");
+            Console.WriteLine(str);
+
+            SayHello obj2 = () => { Console.WriteLine("Hello Lambda Expression"); };
+            obj2.Invoke();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
