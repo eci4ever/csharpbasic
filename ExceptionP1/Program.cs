8bf4d5d786782fa7cf88436fd0b6446f5056f90a
eci4ever <eci4ever@gmail.com>
2019-11-18 10:06:40 +0800

    Learn Exception

---
+++ ExceptionP1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExceptionP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Enter the 1st Number:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second Number:");
+            int y = int.Parse(Console.ReadLine());
+            int z = x / y;
+            Console.WriteLine("The result is : " + z);
+            }
+            catch (DivideByZeroException ex1)
+            {
+                Console.WriteLine("Error " + ex1.Message);
+            }
+            catch (FormatException ex2)
+            {
+                Console.WriteLine("Error : " + ex2.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.Message);
+            }
+            Console.WriteLine("End of Program!!");
+        }
+    }
+}
