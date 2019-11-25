6eb2e6e27ac6fcde52dc5023b5a461682aecb644
eci4ever <eci4ever@gmail.com>
2019-11-25 10:42:52 +0800

    learn multithreading

---
+++ MultiThreadingP2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadingP2
+{
+    class Program
+    {
+        static void Test1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Test1 : "+ i);
+            }
+            Console.WriteLine("Thread 1 is exiting");
+        }
+        static void Test2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Test2 : "+ i);
+                if (i == 20)
+                {
+                    Console.WriteLine("Thread 2 Sleep");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Thread started");
+                }
+            }
+            Console.WriteLine("Thread 2 is exiting");
+        }
+        static void Test3()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Test3 : "+ i);
+            }
+            Console.WriteLine("Thread 3 is exiting");
+        }
+        static void Main(string[] args)
+        {
+            // Thread T1 = new Thread(Test1);
+            // Thread T2 = new Thread(Test2);
+            // Thread T3 = new Thread(Test3);
+            // T1.Start(); T2.Start(); T3.Start();
+            //
+            //ThreadStart is a delegate
+            //ThreadStart obj = new ThreadStart(Test1);
+            //ThreadStart obj = Test3;
+            //ThreadStart obj = delegate () { Test2(); };
+            ThreadStart obj = () => Test1();
+            Thread t1 = new Thread(obj);
+            t1.Start();
+            Console.WriteLine("Main thread Exiting");
+        }
+    }
+}
---
+++ MultiThreadingP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadingP3
+{
+    class Program
+    {
+        static void Test(Object max)
+        {
+            int num = Convert.ToInt32(max);
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Test: "+ i);
+            }
+            Console.WriteLine("Thread is exiting");
+        }
+        static void Main(string[] args)
+        {
+            ParameterizedThreadStart obj = new ParameterizedThreadStart(Test);
+            Thread t1 = new Thread(obj);
+            //Non type safe method to pass int
+            t1.Start(20);
+            //Test(30);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
