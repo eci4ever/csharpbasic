476975e667a666b3181596097e92067771ed7025
eci4ever <eci4ever@gmail.com>
2019-11-26 15:03:57 +0800

    learn collection

---
+++ CollectionsP2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System;
+
+namespace CollectionsP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht1 = new Hashtable();
+            ht1.Add("name", "Eci4ever");
+            ht1.Add("Salary", 3000.50);
+            ht1.Add("Dept", "IT System");
+
+            foreach (var item in ht1.Keys)
+            {
+                Console.WriteLine($"{item} : " + ht1[item]);
+            }
+
+            string str1 = "test";
+            Console.WriteLine(str1.GetHashCode());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CollectionsP3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> li = new List<int>();
+            li.Add(400); li.Add(500);
+            li.Add(100); li.Add(200); li.Add(300);
+            li.Sort();
+
+            foreach (var item in li)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
