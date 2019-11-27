9568e2263b1679d44302ecccbb594fc51bb04e46
eci4ever <eci4ever@gmail.com>
2019-11-27 10:06:20 +0800

    Learn collection p4

---
+++ CollectionsP4a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CollectionsP4a
+{
+    class Program
+    {
+        public static bool Compare<T>(T a, T b)
+        {
+            if (a.Equals(b))
+                return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            var a = Compare<int>(10,10);
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ CollectionsP4b/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CollectionsP4b
+{
+    class Generic2<T>
+    {
+        public void Add(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 + d2);
+        }
+        public void Sub(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 - d2);
+        }
+        public void Mul(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 * d2);
+        }
+        public void Div(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 / d2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic2<int> gn = new Generic2<int>();
+            gn.Add(10,10);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
