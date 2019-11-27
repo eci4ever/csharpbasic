b4cf8be8a53e9b66cf65eaf23338637103b060e8
eci4ever <eci4ever@gmail.com>
2019-11-27 15:14:16 +0800

    learn collecton

---
+++ CollectionsP5a/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CollectionsP5a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> dt = new Dictionary<string,object>();
+
+            dt.Add("name", "Eci4ever");
+            dt.Add("Salary", 3000.50);
+            dt.Add("Dept", "IT System");
+
+            foreach (var item in dt.Keys)
+            {
+                Console.WriteLine(item + " : " + dt[item]);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CollectionsP5b/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsP5b
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            Student s1 = new Student {
+                Id = 101,
+                Name = "Joe",
+                Location = "Kuala Lumpur"
+            };
+            Student s2 = new Student {
+                Id = 102,
+                Name = "Doe",
+                Location = "Kedah"
+            };
+            Student s3 = new Student {
+                Id = 103,
+                Name = "Moe",
+                Location = "Melaka"
+            };
+
+            students.Add(s1); students.Add(s2); students.Add(s3); 
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Id} : {item.Name} : {item.Location}");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
