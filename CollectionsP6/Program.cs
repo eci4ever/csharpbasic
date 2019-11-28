0afe36b08cdb9da412c2c49de4a06bd2e0f848da
eci4ever <eci4ever@gmail.com>
2019-11-28 15:43:31 +0800

    learn enumerator

---
+++ CollectionsP6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsP6
+{
+    class Program
+    {
+        class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Class { get; set; }
+            public float Mark { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Student s1 = new Student() { Id = 103, Name = "Saiful", Class = "F01", Mark = 88.5f };
+            Student s2 = new Student() { Id = 101, Name = "Amar", Class = "F01", Mark = 74.5f };
+            Student s3 = new Student() { Id = 105, Name = "Danish", Class = "F01", Mark = 90.5f };
+            Student s4 = new Student() { Id = 104, Name = "Mariam", Class = "F01", Mark = 95.5f };
+            Student s5 = new Student() { Id = 102, Name = "Siti", Class = "F01", Mark = 77.5f };
+
+            List<Student> students = new List<Student>() { s1, s2, s3, s4, s5 };
+
+            //students.Sort((s1,s2) => s1.Id.CompareTo(s2.Id)); Sort by ID using comparison delegate
+            students.Sort((s1,s2) => s1.Name.CompareTo(s2.Name));
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Id} | {item.Name} | {item.Class} | {item.Mark}");
+            }
+
+
+            int[] myArr = {20, 10, 5, 50 , 30, 40 };
+            List<int> listInt = new List<int>();
+            listInt.AddRange(myArr);
+            listInt.Sort();
+            foreach (var item in listInt)
+            {
+                Console.Write(item + " ");
+            }
+           // Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CollectionsP8/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+using System.Diagnostics.Tracing;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsP8
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public double Salary { get; set; }
+    }
+    //Let define our own collection
+    public class Organization : IEnumerable
+    {
+        List<Employee> emps = new List<Employee>();
+        public void Add(Employee Emp)
+        {
+            emps.Add(Emp);
+        }
+        public int Count
+        {
+            get { return emps.Count; }
+        }
+        public Employee this[int index]
+        {
+            get { return emps[index]; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new OrganizationEnumerator(this);
+        }
+    }
+    public class OrganizationEnumerator : IEnumerator
+    {
+        Organization orgColl;
+        int currentIndex;
+        Employee currentEmployee;
+        public OrganizationEnumerator(Organization org)
+        {
+            orgColl = org;
+            currentIndex = -1;
+        }
+        public object Current
+        {
+            get 
+            {
+                return currentEmployee;
+            }
+        }
+        public bool MoveNext()
+        {
+            if (++currentIndex >= orgColl.Count)
+                return false;
+            else
+                currentEmployee = orgColl[currentIndex];
+            return true;
+        }
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Employee> employees = new List<Employee>();
+            Organization employees = new Organization();
+            employees.Add( new Employee { Id = 101, Name = "Johan", Job = "Pengurus", Salary = 5000.00 });
+            employees.Add( new Employee { Id = 102, Name = "Sarah", Job = "IT", Salary = 2000.00 });
+            employees.Add( new Employee { Id = 103, Name = "Abu", Job = "HR", Salary = 4000.00 });
+            employees.Add( new Employee { Id = 104, Name = "Jamal", Job = "Operasi", Salary = 1000.00 });
+            employees.Add( new Employee { Id = 105, Name = "Kamal", Job = "Operasi", Salary = 1500.00 });
+
+            foreach (Employee item in employees)
+            {
+                Console.WriteLine($"{item.Id} {item.Name} {item.Job} {item.Salary}");
+            }
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
